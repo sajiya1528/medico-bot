97ebda5b246c297f38b45e6265395e4bb5260644
sajiya1528 <sajiyanazir28@gmail.com>
2026-01-19 14:35:00 +0530

    Remove backend implementation of Medico Bot, including all related files and configurations.

--- server/routes/aiRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { chat, symptomChecker } from '../controllers/aiController';
-
-const router = Router();
-
-router.post('/chat', chat);
-router.post('/symptoms', symptomChecker);
-
-export default router;
